3eb7dfb8b8a878f09aeeab6ed36993256e4abd59
Micheal Colhoun <micheal@colhountech.com>
2022-12-18 13:53:45 +0000

    New AcrylicProcessBar with Grey Background

---
+++ AcrylicUI/Controls/AcrylicProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using AcrylicUI.Resources;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AcrylicUI.Controls
+{
+    public partial class AcrylicProgressBar : ProgressBar
+    {
+        public AcrylicProgressBar()
+        {
+            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            this.BackColor = Colors.GreyBackground;            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.Clear(Colors.GreyBackground);
+
+            int width = (int)((double)this.Value / this.Maximum * this.Width);
+
+            e.Graphics.FillRectangle(Brushes.Lime, 0, 0, width, this.Height); 
+            
+        }
+    }
+}
